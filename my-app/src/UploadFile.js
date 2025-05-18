e47ee891c37442e4e088d4374b20d01104117e73
rahul teja <padmamrahulteja6011@gmail.com>
2025-05-18 18:44:14 +0530

    file type added

--- my-app/src/UploadFile.js
+++ my-app/src/UploadFile.js
@@ -2,36 +2,43 @@ import React, { useState } from 'react';
 import './UploadFile.css';
 
 const UploadForm = ({ onFileUpload }) => {
-  const [selectedFile, setSelectedFile] = useState(null);
+    const [selectedFile, setSelectedFile] = useState(null);
 
-  const handleChange = (e) => {
-    setSelectedFile(e.target.files[0]);
-  };
+    const handleChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (selectedFile) {
-      onFileUpload(selectedFile);
-      setSelectedFile(null); // reset after upload
-    }
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (selectedFile) {
+            onFileUpload(selectedFile);
+            setSelectedFile(null);
+        }
+    };
 
-  return (
-    <form onSubmit={handleSubmit} className="upload-form">
-      <label className="file-input-label">
-        Choose File
-        <input type="file" onChange={handleChange} className="hidden-input" />
-      </label>
+    return (
+        <form onSubmit={handleSubmit} className="upload-form">
+            <div className="upload-file-div">
+                <label className="file-input-label">
+                    Choose File
+                    <input type="file" onChange={handleChange} className="hidden-input" />
+                </label>
 
-      <span className="file-name">
-        {selectedFile ? selectedFile.name : 'No file selected'}
-      </span>
+                <span className="file-name">
+                    {selectedFile ? selectedFile.name : 'No file selected'}
+                </span>
 
-      <button type="submit" className="upload-button" disabled={!selectedFile}>
-        Upload
-      </button>
-    </form>
-  );
+                <button type="submit" className="upload-button" disabled={!selectedFile}>
+                    Upload
+                </button>
+            </div>
+
+            <span className="file-restriction-note">
+                Files of type (.png, .jpeg, .jpg, .json, .txt) only allowed
+            </span>
+        </form>
+
+    );
 };
 
 export default UploadForm;
